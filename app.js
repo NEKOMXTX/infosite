1b171131bdc6eb341707a7636d3a7f9883dba590
nekomxtx <pockervlad@gmail.com>
2023-07-06 12:02:08 +0300

    added buffer/streams

--- app.js
+++ app.js
@@ -5,47 +5,48 @@ const path = require('path');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log('Sever request');
-    console.log('Just for test');
+  console.log('Server request');
+  console.log('Just for test');
 
-    res.setHeader('Content-Type', 'text/html' )
+  res.setHeader('Content-Type', 'text/html');
 
-    const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+  const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
 
-    let basePath = '';
+  let basePath = '';
 
-    switch(req.url) {
-        case '/':
-            basePath = createPath('homepage');
-            res.statusCode = 200;
-            break;
-        case '/products':
-            basePath = createPath('products');
-            res.statusCode = 200;
-            break;
-        case '/contact':
-            basePath = createPath('contact');
-            res.statusCode = 200;
-            break;
-        default:
-            basePath = createPath('error');
-            res.statusCode = 404;
-            break;
-    }
+  switch (req.url) {
+    case '/':
+      basePath = createPath('homepage');
+      res.statusCode = 200;
+      break;
+    case '/products':
+      basePath = createPath('products');
+      res.statusCode = 200;
+      break;
+    case '/contact':
+      basePath = createPath('contact');
+      res.statusCode = 200;
+      break;
+    default:
+      basePath = createPath('error');
+      res.statusCode = 404;
+      break;
+  }
 
-    fs.readFile(basePath, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            res.end();
-        }
-        else {
-            res.write(data);
-            res.end();
-        }
-    })
+  const fileStream = fs.createReadStream(basePath);
+
+  fileStream.on('open', () => {
+    res.statusCode = 200;
+    fileStream.pipe(res);
+  });
+
+  fileStream.on('error', (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end();
+  });
 });
 
 server.listen(PORT, 'localhost', (error) => {
-    error ? console.log(error) : console.log(`listening port ${PORT}`);
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
